e09ce39ab977df210a486b1af05c7c64c4485aec
gzzyworld420 <iaracena@udesa.edu.ar>
2023-10-28 13:23:19 -0300

    subiendo 18-particles

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 THREE.ColorManagement.enabled = false
 
@@ -55,6 +56,23 @@ gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(hemisphereLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(pointLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(rectAreaLight, 'intensity').min(0).max(10).step(0.001)
+
+
+// Helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
 
 /**
  * Objects
